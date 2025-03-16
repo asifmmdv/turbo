495b429d3955391a89fe0bced44c7b79d34059f3
asifmmdv <mamedov_asif@icloud.com>
2025-03-16 17:35:53 +0400

    fixprices

--- js/basket.js
+++ js/basket.js
@@ -6,39 +6,62 @@ let basket = []
 const likedDiv = document.getElementById('likedDiv')
 function showBasket() {
     likedDiv.innerHTML = '';
+    let totalCount = 0; 
+    let totalPrice = 0; 
+    let discountedPrice = 0; 
+    let totalDiscount = 0; 
+
     basket.map((item, i) => {
-        let totalPrice = item.count * item.price
-        totalPrice = discount(item, totalPrice)
+        let carPrice = item.count * item.price; 
+        totalCount += item.count; 
+        totalPrice += carPrice; 
+        discountedPrice += discount(item, carPrice); 
+    });
+
+    totalDiscount = totalPrice - discountedPrice; 
+
+    document.getElementById('mehsulsayi').innerHTML = `Məhsul sayı: ${totalCount}`;
+    document.getElementById('umimiqiymet').innerHTML = `Ümumi qiymət: ${totalPrice} ${basket[0]?.currency || ''}`;
+    document.getElementById('endirimliqiymet').innerHTML = `Ümumi endirimli qiymət: ${discountedPrice} ${basket[0]?.currency || ''}`;
+    document.getElementById('endirim').innerHTML = `Endirim: ${totalDiscount} ${basket[0]?.currency || ''}`;
+
+    basket.map((item, i) => {
+        let carPrice = item.count * item.price; 
+
         likedDiv.innerHTML += `
         <article class="w-[95%] h-[140px] text-center m-4 overflow-hidden shadow-2xl border-[rgb(202,16,22)] flex border-[1px] flex-row items-center rounded-[8px]" id="x${i}">
             <div class="max-w-[40%] h-[100%] overflow-hidden">
-               <img class="h-[100%] object-cover" src="${item.images[0]}" alt="avto" />
+                <img class="h-[100%] object-cover" src="${item.images[0]}" alt="avto" />
             </div>
-            <div  class="w-[60%] bg-white p-2 h-[140px]">
-               <h2 class="font-bold">${item.price} ${item.currency}</h2>
-               <h6>${item.brand} ${item.model}</h6>
-               <p>${item.year}, ${item.engine}, ${item.odometer}${item.odometerUnit}</p>
-               <span>${item.city}, ${item.dates}</span>
-               <div id="totalprice" class="bg-[rgb(202,16,22)] text-white">${totalPrice} ${item.currency}</div>  
+            <div class="w-[60%] bg-white p-2 h-[140px]">
+                <h2 class="font-bold">${item.price} ${item.currency}</h2>
+                <h6>${item.brand} ${item.model}</h6>
+                <p>${item.year}, ${item.engine}, ${item.odometer}${item.odometerUnit}</p>
+                <span>${item.city}, ${item.dates}</span>
+                <div id="carprice" class="bg-[rgb(202,16,22)] text-white">${carPrice} ${item.currency}</div>  
             </div>
-            <div class="h-[30%] shadow-2xl flex flex-col  justify-center gap-1">
-            <button onclick="basketCount(${item.id}, -1)" class="border-[1px] text-[rgb(202,16,22)] border-[#dfe3e7] w-[25px] h-[25px] rounded-[5px] mx-[5px]">-</button>
-                <span class="text-[rgb(202,16,22)]" id="num">${item.count} </span>
+            <div class="h-[30%] shadow-2xl flex flex-col justify-center gap-1">
+                <button onclick="basketCount(${item.id}, -1)" class="border-[1px] text-[rgb(202,16,22)] border-[#dfe3e7] w-[25px] h-[25px] rounded-[5px] mx-[5px]">-</button>
+                <span class="text-[rgb(202,16,22)]" id="num">${item.count}</span>
                 <button onclick="basketCount(${item.id}, 1)" class="border-[1px] border-[#dfe3e7] w-[25px] h-[25px] text-[rgb(202,16,22)] rounded-[5px] mx-[5px]">+</button>
-                </div>
-                <i onclick="basketdenSil(${item.id})" class="fa-solid fa-trash text-2xl text-[rgb(202,16,22)] mr-2"></i>
-            </article>
-      `
-
-    })
+            </div>
+            <i onclick="basketdenSil(${item.id})" class="fa-solid fa-trash text-2xl text-[rgb(202,16,22)] mr-2"></i>
+        </article>
+        `;
+    });
 }
-showBasket()
 
-function discount(item, totalPrice){
-    if (item.count >= 10){
-        return totalPrice = totalPrice - item.price
+function basketdenSil(id) {
+    basket = basket.filter(item => item.id !== id); 
+    showBasket(); 
+}
+    showBasket()
+    
+    function discount(item, carPrice){
+        if (item.count >= 10){
+        return  carPrice = 0.9*carPrice
     }
-    return totalPrice
+    return carPrice
 }
 
 function addToBasket(id) {
@@ -73,3 +96,4 @@ function basketCount(id, x) {
     showBasket()
 }
 
+i
